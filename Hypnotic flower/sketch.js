cd1133ceb652fd3a93553b5c4f710ec7fea3f8d3
Supriyo Pramanik <supriyopramanik.2006@gmail.com>
2025-10-21 12:14:01 +0530

    done hypotic flower

--- Hypnotic flower/sketch.js
+++ Hypnotic flower/sketch.js
@@ -1,7 +1,22 @@
+let flowers = []; 
+let num = 15;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
+  angleMode(DEGREES);
+  for (let i=0; i<num; i++) {
+
+    flowers[i] = new Flower(140 - i*10, 100, 15, 7, (i+1)*0.1);
+
+  }
+  
 }
 
 function draw() {
-  background(220);
+  background(22);
+  translate(width/2, height/2);
+  for (let i=0; i<num; i++) {
+    flowers[i].display();
+  }
+  
 }
